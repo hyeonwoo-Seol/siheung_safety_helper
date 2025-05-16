0963ef257a2fefc5d9c37b4ec4b11dd2610f1454
kjm63517 <kjm63517@tukorea.ac.kr>
2025-05-16 16:08:14 +0900

    gradle 수정

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -53,4 +54,7 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    implementation("androidx.core:core-splashscreen:1.0.1")
+    implementation("com.google.firebase:firebase-auth:23.2.0")
 }
